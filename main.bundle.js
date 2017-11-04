456ee979d086c586b422b6494d6b870964ea26ca
Thomas Guyard <thomas.guyard@gmail.com>
2017-11-04 17:27:40 +0100

    first commit

---
+++ main.bundle.js
@@ -0,0 +1,321 @@
+webpackJsonp(["main"],{
+
+/***/ "../../../../../src/$$_gendir lazy recursive":
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncatched exception popping up in devtools
+	return Promise.resolve().then(function() {
+		throw new Error("Cannot find module '" + req + "'.");
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "../../../../../src/$$_gendir lazy recursive";
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_survey_angular__ = __webpack_require__("../../../../survey-angular/survey.angular.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_survey_angular___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_survey_angular__);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+__WEBPACK_IMPORTED_MODULE_1_survey_angular__["Survey"].cssType = "bootstrap";
+var AppComponent = (function () {
+    function AppComponent() {
+        this.json = {
+            completeText: "Valider",
+            pages: [
+                {
+                    elements: [
+                        {
+                            type: "dropdown",
+                            name: "groupe",
+                            title: "Votre groupe",
+                            isRequired: true,
+                            choices: [
+                                "Dumont d'Urville",
+                                "Le Bon Conseil",
+                                "Notre Dame de la Gare",
+                                "Notre Dame du Liban",
+                                "Notre Dame du Rosaire",
+                                "Saint Étienne du Mont",
+                                "Saint Germain des Prés",
+                                "Saint Jacques du Haut Pas",
+                                "Saint Jean Baptiste de la Salle",
+                                "Saint Médard",
+                                "Saint Pierre de Montrouge"
+                            ]
+                        },
+                        {
+                            type: "matrixdynamic",
+                            name: "jeunes",
+                            title: "Liste des jeunes qui viennent",
+                            isRequired: true,
+                            columns: [
+                                {
+                                    name: "nom",
+                                    title: "Prenom et Nom",
+                                    isRequired: true
+                                },
+                                {
+                                    name: "responsabilite",
+                                    title: "Responsabilité",
+                                    choices: [
+                                        {
+                                            value: "animation",
+                                            text: "Responsable animation"
+                                        },
+                                        {
+                                            value: "budget",
+                                            text: "Responsable budget"
+                                        },
+                                        {
+                                            value: "communication",
+                                            text: "Responsable communication"
+                                        },
+                                        {
+                                            value: "documentation",
+                                            text: "Responsable documentation"
+                                        },
+                                        {
+                                            value: "hébergement",
+                                            text: "Responsable hébergement"
+                                        },
+                                        {
+                                            value: "intendance",
+                                            text: "Responsable intendance"
+                                        },
+                                        {
+                                            value: "matériel",
+                                            text: "Responsable matériel"
+                                        },
+                                        {
+                                            value: "santé",
+                                            text: "Responsable santé"
+                                        },
+                                        {
+                                            value: "spirituelle",
+                                            text: "Responsable vie spirituelle"
+                                        }
+                                    ],
+                                    cellType: "dropdown",
+                                    isRequired: true
+                                },
+                                {
+                                    name: "annee",
+                                    title: "Année",
+                                    choices: [
+                                        "1",
+                                        "2",
+                                        "3"
+                                    ],
+                                    cellType: "radiogroup",
+                                    isRequired: true
+                                }
+                            ],
+                            choices: [
+                                1,
+                                2,
+                                "3"
+                            ],
+                            cellType: "text",
+                            rowCount: 5,
+                            addRowText: "Ajouter un jeune",
+                            removeRowText: "Enlever"
+                        },
+                        {
+                            type: "matrixdynamic",
+                            name: "chefs",
+                            title: "Liste des chefs qui viennent",
+                            isRequired: true,
+                            columns: [
+                                {
+                                    name: "nom",
+                                    title: "Prénom et Nom",
+                                    isRequired: true
+                                },
+                                {
+                                    name: "Adresse email",
+                                    isRequired: true
+                                },
+                                {
+                                    name: "Je participe à l'annimation",
+                                    choices: [
+                                        {
+                                            value: "cap",
+                                            text: "Du jeu 'Approfondissement du cap'"
+                                        },
+                                        {
+                                            value: "grand_jeu",
+                                            text: "Du grand jeu"
+                                        },
+                                        {
+                                            value: "veillée",
+                                            text: "De la veillée"
+                                        },
+                                        {
+                                            value: "messe",
+                                            text: "De la messe"
+                                        },
+                                        {
+                                            value: "responsabilités",
+                                            text: "De l'atelier sur les responsabilités"
+                                        }
+                                    ],
+                                    cellType: "dropdown",
+                                    isRequired: true
+                                }
+                            ],
+                            choices: [
+                                1,
+                                2,
+                                3,
+                                4,
+                                5
+                            ],
+                            cellType: "text",
+                            addRowText: "Ajouter un chef",
+                            removeRowText: "Enlever"
+                        },
+                        {
+                            type: "comment",
+                            name: "régime",
+                            title: "Régimes et restrictions alimentaires dans la Caravane / Flotille",
+                            rows: 2
+                        },
+                        {
+                            type: "comment",
+                            name: "comment",
+                            title: "Un commentaire, une petite blague ... :)"
+                        }
+                    ],
+                    innerIndent: 2,
+                    name: "page1",
+                    title: "Inscription WEECAP 18/19 novembre"
+                }
+            ]
+        };
+    }
+    AppComponent.prototype.ngOnInit = function () {
+        var survey = new __WEBPACK_IMPORTED_MODULE_1_survey_angular__["ReactSurveyModel"](this.json);
+        survey.onComplete.add(function (survey) {
+            console.log('SEND RESULT !');
+            survey.sendResult('4234bd81-d232-4dd1-9890-827c176a85d9');
+        });
+        __WEBPACK_IMPORTED_MODULE_1_survey_angular__["SurveyNG"].render("surveyElement", { model: survey });
+    };
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+        selector: 'app-root',
+        template: "\n  <div class=\"survey-container contentcontainer codecontainer\"><div id=\"surveyElement\"></div></div>"
+    })
+], AppComponent);
+
+//# sourceMappingURL=app.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("../../../platform-browser/@angular/platform-browser.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["L" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */]
+        ],
+        providers: [],
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]]
+    })
+], AppModule);
+
+//# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/environments/environment.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+// The file contents for the current environment will overwrite these during build.
+var environment = {
+    production: false
+};
+//# sourceMappingURL=environment.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/main.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__("../../../platform-browser-dynamic/@angular/platform-browser-dynamic.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__("../../../../../src/app/app.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__("../../../../../src/environments/environment.ts");
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_19" /* enableProdMode */])();
+}
+Object(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */])
+    .catch(function (err) { return console.log(err); });
+//# sourceMappingURL=main.js.map
+
+/***/ }),
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("../../../../../src/main.ts");
+
+
+/***/ })
+
+},[0]);
+//# sourceMappingURL=main.bundle.js.map
